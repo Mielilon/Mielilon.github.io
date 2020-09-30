2cecc7d10a4e4656c3f1971333bbc42a6172b48a
Mielilon <daria.mg354@mail.ru>
2020-09-30 19:53:40 +0300

    Сделана страница одной из услуг

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,24 @@ $(document).on('click','.js-videoPoster',function(e) {
     wrapper.addClass('video__wrapper_active');
     iframe.attr('src',src);
   }
+
+
+
+//открытие-закрытие модального окна
+$('.equipment-cards__item').click(function(){
+        $('.equipment-popup').addClass('equipment-popup__active');
+});
+
+$('.close').click(function(){
+  $('.equipment-popup').removeClass('equipment-popup__active');
+});
+
+jQuery(function($){
+	$('.equipment-popup').mouseup(function (e){ 
+		var popup = $("#equipment-popup"); 
+		if (!popup.is(e.target) 
+		    && popup.has(e.target).length === 0) { 
+        $('.equipment-popup').removeClass('equipment-popup__active');
+		}
+	});
+});
